b744f390cee4527b5bf99e3dfa3197a8b77e74a3
mdjafarnadaf25 <mahammedjafaranadaf@gmail.com>
2025-05-07 12:12:08 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/anushreebarman/App.java
@@ -0,0 +1,13 @@
+package com.anushreebarman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
